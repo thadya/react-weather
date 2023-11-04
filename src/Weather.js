00756ca3846287102f47c099ea9e56c4a53fb7a2
thadya <141648684+thadya@users.noreply.github.com>
2023-11-04 20:42:14 +0000

    changes

--- src/Weather.js
+++ src/Weather.js
@@ -48,13 +48,14 @@ export default function Weather(props) {
                 autoFocus="on"
                 onChange={handleCityChange}
               />
-            </div>
+            
             <div className="col-3">
               <input
                 type="submit"
                 value="Search"
                 className="btn btn-primary w-100"
               />
+            </div>
             </div>
           </div>
         </form>
