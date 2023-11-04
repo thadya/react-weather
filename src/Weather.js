a7497abe2b0024e58aedeea91992db72cacc7fdb
thadya <141648684+thadya@users.noreply.github.com>
2023-11-04 20:53:05 +0000

    chages

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./WeatherInfo";
-import "./Weather.css";
+import "./Weather.css"; 
 
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
