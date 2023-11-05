356836248e955c66862b14886e91a7668af677ed
thadya <141648684+thadya@users.noreply.github.com>
2023-11-05 17:16:08 +0000

    changes

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -12,7 +12,7 @@ export default function WeatherInfo(props) {
           </li>
           <li className="text-capitalize">{props.data.description}</li>
         </ul>
-        <div className="row mt-3">
+        <div className="row mt-4">
           <div className="col-6">
             <div className="d-flex">
               <img src={props.data.icon_url} alt={""} className="float-left" />
