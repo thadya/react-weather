495507cd9f57d28fab634a27fbe1eef108beb810
thadya <141648684+thadya@users.noreply.github.com>
2023-11-05 13:44:05 +0000

    Update Weather.js

--- src/Weather.js
+++ src/Weather.js
@@ -49,7 +49,7 @@ export default function Weather(props) {
                 <input
                   type="submit"
                   value="Search"
-                  className="btn btn-primary w-100"
+                  className="btn btn-primary"
                 />
               </div>
             </div>
