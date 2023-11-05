b0710d55cac544dcc8404abdbbb7f00b1c3c1022
thadya <141648684+thadya@users.noreply.github.com>
2023-11-05 00:36:02 +0000

    cb

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -3,35 +3,31 @@ import FormattedDate from "./FormattedDate";
 import WeatherTemperature from "./WeatherTemperature";
 
 export default function WeatherInfo(props) {
-  return (
-    <div className="WeatherInfo">
-      <h1>{props.data.city}</h1>
-      <ul>
-        <li>
-          <FormattedDate date={props.data.date} />
-        </li>
-        <li className="text-capitalize">{props.data.description}</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-        <div className="d-flex">
-            <img
-              src={props.data.iconUrl}
-              alt={props.data.description}
-              className="float-left "
-            />
-            <div className="float-left">
-              <WeatherTemperature celsius={props.data.temperature} />
+    return (
+      <div className="WeatherInfo">
+        <h1>{props.data.city}</h1>
+        <ul>
+          <li>
+            <FormattedDate date={props.data.date} />
+          </li>
+          <li className="text-capitalize">{props.data.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="d-flex">
+              <img src={props.data.icon_url} alt={""} className="float-left" />
+              <div className="float-left">
+                <WeatherTemperature celsius={props.data.temperature} />
               </div>
             </div>
           </div>
-        <div className="col-6">
-          <ul>
-            <li>Humidity: {props.data.humidity}%</li>
-            <li>Wind: {Math.round(props.data.wind)} km/h</li>
-          </ul>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {props.data.humidity} %</li>
+              <li>Wind: {props.data.wind.speed} km/h</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
-}
+    );
+  }
