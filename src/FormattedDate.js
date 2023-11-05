7f185d37d2df73ee480a23c50ff0c2b28fef2e01
thadya <141648684+thadya@users.noreply.github.com>
2023-11-05 01:52:21 +0000

    Update FormattedDate.js

--- src/FormattedDate.js
+++ src/FormattedDate.js
@@ -1,12 +1,13 @@
 export default function FormattedDate(props) {
   let days = [
+    "Sunday",
     "Monday",
     "Tuesday",
     "Wednesday",
     "Thursday",
     "Friday",
     "Saturday",
-    "Sunday",
+    
   ];
   let day = days[props.date.getDay()];
   let hours = props.date.getHours();
